9b43d6b001e6d914b461bc851d498c9e9e3da0c1
Violet <vi@violet.wtf>
2020-08-07 17:10:06 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import './App.css';
+import Creators from './Creators.json';
+import Experience from './components/Experience';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// Logos
+import GraserLogo from './assets/graser.jpg';
+import KiingLogo from './assets/kiing.jpg';
+import AntLogo from './assets/ant.jpg';
+import WilburLogo from './assets/wilbur.jpg';
+
+function App() 
+{
+    const [name, setName] = useState("violet mckinney");
+    const [link, setLink] = useState("https://violet.wtf");
+
+    function nl(name: string, link: string)
+    {
+        setName(name);
+        setLink(link);
+    }
+
+    const creators = Creators as any;
+
+    const creatorCount = Object.keys(Creators).length;
+
+    let videos = 0;
+    let viewsThousands = 0;
+    
+    for (const key of Object.keys(creators))
+    {
+        if (key.startsWith("_"))
+        {
+            continue;
+        }
+
+        const creator = creators[key];
+
+        videos += creator.viewsThousands.length;
+        viewsThousands += creator.viewsThousands.reduce((a: number, b: number) => a + b);
+    }
+
+    const viewsMillions = (viewsThousands / 1000).toFixed(1);
+
+    return (
+        <div>
+            <div className="nameArea">
+                <p className="name">
+                    <p><strong className="actualName"><a href={link}>{name} </a></strong></p>
+                    is the developer behind 
+                    <strong> {videos}</strong> videos, 
+                    by <strong>{creatorCount}</strong> creators, 
+                    with <strong>{viewsMillions} million</strong> combined views.
+                </p>
+                <div className="socials">
+                <span 
+                        className="social" 
+                        onClick={_ => nl("vi@violet.wtf", 'mailto:vi@violet.wtf')}>email
+                    </span>
+                    <span> | </span>
+                    <span 
+                        className="social" 
+                        onClick={_ => nl("violet#3993", 'https://violet.wtf')}>discord
+                    </span>
+                    <span> | </span>
+                    <span 
+                        className="social"
+                        onClick={_ => nl("@violet_wtf", 'https://twitter.com/violet_wtf')}>twitter 
+                    </span>
+                    <span> | </span>
+                    <span 
+                        className="social"
+                        onClick={_ => nl("@violetwtf", 'https://github.com/violetwtf')}>github 
+                    </span>
+                </div>
+            </div>
+            <div className="experienceArea">
+                <Experience 
+                    gradientColors={['#aba514', '#a56b01', '#393939']}
+                    logo={GraserLogo}
+                    creator={Creators.graser}
+                />
+                <Experience
+                    gradientColors={['#e49a64', '#a5019a', '#00bbff']}
+                    logo={KiingLogo}
+                    creator={Creators.kiing}
+                />
+                <Experience
+                    gradientColors={['#00bbff', '#01a57e', '#4a9fb0']}
+                    logo={AntLogo}
+                    creator={Creators.ant}
+                />
+                <Experience
+                    gradientColors={['#167bc9', '#4f00a6', '#5f98a1']}
+                    logo={WilburLogo}
+                    creator={Creators.wilbur}
+                />
+            </div>
+        </div>
+    );
+
+}
+
+export default App;
---
+++ src/components/Experience.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+export default function Experience(props: any)
+{
+
+    const { creator } = props;
+
+    const background = 
+    {
+        background: `linear-gradient(156deg, ` + 
+        `${props.gradientColors[0]} 0%, ` + 
+        `${props.gradientColors[1]} 25%, ` + 
+        `${props.gradientColors[2]} 100%)`
+    }
+
+    const currentYear = creator.current 
+        ? 'Present' 
+        : (
+            creator.years.length > 1
+            ? creator.years[1]
+            : false
+        );
+
+    const totalViewsThousands = creator.viewsThousands.reduce((a: number, b: number) => a + b);
+
+    let views: string;
+
+    if (totalViewsThousands >= 1000)
+    {
+        views = totalViewsThousands / 1000 + " million";
+    }
+    else
+    {
+        views = totalViewsThousands + " thousand";
+    }
+
+    const isOneVideo = creator.viewsThousands.length === 1;
+
+    return (
+        <div className='experience' style={background}>
+            <div className='expContent'>
+                <img className='logo' src={props.logo} />
+                <div className='expText'>
+                    <p>
+                        <b className='expName'>
+                            <a className='expLink' href={'https://youtube.com/' + creator.yt}>
+                                { creator.name }
+                            </a>
+                        </b>
+                    </p>
+                    <p>
+                        <b>{creator.subs} </b> 
+                        subscribers &middot; collaborated on 
+                        <b> {creator.viewsThousands.length}</b> video{isOneVideo ? '' : 's'}
+                        {creator.current ? ', and counting, ' : ' '}
+                        with <b>{views}</b>{isOneVideo ? '' : ' total'} views.
+                        </p>
+                    <span className='expYear'>
+                        {creator.years[0]} {currentYear ? ` - ` + currentYear : ''}
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+
+}
